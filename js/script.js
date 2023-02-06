26411a3c694454ac238a18770096b0fbed9ebcb6
fairy <fairyplacesss@gmail.com>
2023-02-06 13:50:28 +0500

    Анимация скрытия шапки сайта

---
+++ js/script.js
@@ -0,0 +1,39 @@
+"use-strict";
+
+const hideNav = () => {
+	const hiddenHeaderClassName = "header__hidden";
+	const headerHeight = 92;
+
+	let initialValue = window.scrollY;
+	let header = document.querySelector("header");
+
+  let isItHidden = false;
+
+	window.addEventListener("scroll", (e) => {
+		const scrollY = window.scrollY;
+
+		if (scrollY > headerHeight && scrollY > initialValue) {
+			hideHeader();
+		} else {
+			showHeader();
+		}
+
+		initialValue = scrollY;
+	});
+
+	function hideHeader() {
+    if (!isItHidden) {
+      header.classList.add(hiddenHeaderClassName);
+      isItHidden = true
+    }
+	}
+
+	function showHeader() {
+    if (isItHidden) {
+      header.classList.remove(hiddenHeaderClassName);
+      isItHidden = false
+    }
+	}
+};
+
+hideNav();
