33dee61ec407b5dacb880e5f472225c6a2087364
fairy <fairyplacesss@gmail.com>
2023-02-21 15:24:55 +0500

    Маска для ввода номера телефона

--- js/script.js
+++ js/script.js
@@ -52,8 +52,9 @@ if (mobileMenuIcon) {
 const menuLinks = document.querySelectorAll('a[data-goto]')
 
 if (menuLinks.length > 0){
-  for (const menuLink of menuLinks) {
-    menuLink.addEventListener('click', onMenuLinkClick)
+  for (let i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].addEventListener('click', onMenuLinkClick)
+    
   }
 }
 
@@ -61,9 +62,8 @@ function onMenuLinkClick(e) {
 
   let menuLink = e.target
 
-  if (!e.target.dataset.goto) {
-    menuLink = e.target.closest('a[data-goto]')    
-  }
+  if (!e.target.dataset.goto) menuLink = e.target.closest('a[data-goto]')    
+  
 
   if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
     const gotoBlock = document.querySelector(menuLink.dataset.goto);
@@ -79,15 +79,55 @@ function onMenuLinkClick(e) {
       top: gotoBlockValue,
       behavior: "smooth"
     });
+
     e.preventDefault();
+    
     if (e.target.dataset.goto == '.page__form-section') {
       setTimeout(()=>{
         document.forms[1].elements.phoneNum.focus()
       }, 500)
     }
+
   }
-
-
 }
 
+// Phone Mask
+
+const phoneInputs = document.querySelectorAll('input[data-tel-input]')
+
+function getInputNumbersValue(input) {
+  return input.value.replace(/\D/g, '')
+}
+
+function onPhoneInput(e) {
+  let input = e.target
+  let inputNumbersValue = getInputNumbersValue(input)
+  let formattedInputValue = ""
+
+  if (!inputNumbersValue) return input.value = ''
+
+  if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+    if (inputNumbersValue[0] == "9") return     input.value = `+7 ${inputNumbersValue}`
+    
+    let firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7"
+    formattedInputValue = firstSymbols
+
+    if (inputNumbersValue.length > 1) formattedInputValue += ` (${inputNumbersValue.substring(1, 4)}`
+    
+    if (inputNumbersValue.length >= 5) formattedInputValue += `) ${inputNumbersValue.substring(4, 7)}` 
+
+    if (inputNumbersValue.length >= 8) formattedInputValue += `-${inputNumbersValue.substring(7, 9)}`
+    
+    if (inputNumbersValue.length >= 10) formattedInputValue += `-${inputNumbersValue.substring(9, 11)}` 
+    
+  } else {
+    formattedInputValue = `+${inputNumbersValue.substring(0, 16)}`
+  }
+  input.value = formattedInputValue
+}
+
+for (let i = 0; i < phoneInputs.length; i++) {
+  let input = phoneInputs[i]
+  input.addEventListener('input', onPhoneInput)  
+}
 
